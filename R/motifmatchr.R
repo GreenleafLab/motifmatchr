fa33fe49bf649db5cdecb8880ec457cd35b869ac
Alicia Schep <aschep@stanford.edu>
2017-02-06 16:34:30 -0800

    updated package documentation

--- R/motifmatchr.R
+++ R/motifmatchr.R
@@ -1,6 +1,20 @@
-#' motifmatchr
+#' motifmatchr: Fast Motif Matching in R
 #'
-#' Fast motif matching in R
+#' The motifmatchr package is designed for analyzing many sequences and many
+#' motifs to find which sequences contain which motifs.
+#'
+#' motifmatchr uses the MOODS C++ library (developedby Pasi Rastas,
+#' Janne Korhonen, and Petri Martinmaki) internally for motif matching.
+#'
+#' The primary method of motifmatchr is \code{\link{match_motifs}}, which takes
+#' in motif PWMs/PFMs and genomic ranges or sequences and returns either which
+#' ranges/sequences match which motifs or the positions of the matches.
+#'
+#' Compared with alternative motif matching functions available in Bioconductor
+#' (e.g. matchPWM in Biostrings or searchSeq in TFBSTools), motifmatchr is
+#' designed specifically for the use case of determining whether many different
+#' sequences/ranges contain many different motifs.
+#'
 #' @useDynLib motifmatchr
 #' @docType package
 #' @import Matrix SummarizedExperiment methods
@@ -15,6 +29,7 @@
 #' @importClassesFrom Biostrings DNAString DNAStringSet
 #' @importClassesFrom GenomicRanges GenomicRanges
 #' @importClassesFrom TFBSTools PWMatrix PFMatrix PWMatrixList PFMatrixList
+#' @author Alicia Schep
 #' @name motifmatchr
 NULL
 # > NULL
